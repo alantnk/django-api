6ae765afc99a37ab0392f526d93b0096b7fd4b52
alan <alan@tnk.com>
2025-01-27 12:51:34 -0300

    feat: static/js update

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -1,4 +1,7 @@
-// const href = window.location.href;
-// window.onload = () => {
-//     document.getElementById("domain").textContent = href;
-// };
+function main() {
+    console.log(
+        "That's right, twenty five years into the future. I've always dreamed on seeing the future, looking beyond my years, seeing the progress of mankind. I'll also be able to see who wins the next twenty-five world series."
+    );
+}
+
+window.onload(main);
