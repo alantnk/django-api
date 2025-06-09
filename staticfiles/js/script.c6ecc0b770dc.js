87f13617c29df513a0c992d71b896ddcb0d0d673
alan <alan@tnk.com>
2025-06-08 23:44:15 -0300

    refactor: rm staticfiles folder

--- staticfiles/js/script.c6ecc0b770dc.js
+++
@@ -1,7 +0,0 @@
-function main() {
-    console.log(
-        "That's right, twenty five years into the future. I've always dreamed on seeing the future, looking beyond my years, seeing the progress of mankind. I'll also be able to see who wins the next twenty-five world series."
-    );
-}
-
-window.onload = main;
